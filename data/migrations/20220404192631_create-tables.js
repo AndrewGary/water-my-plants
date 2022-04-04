518b0be2704205f92eaea0375dac7863f386b4e9
Andrew Gary <andrew.gary91@gmail.com>
2022-04-04 14:43:52 -0500

    added migration files

---
+++ data/migrations/20220404192631_create-tables.js
@@ -0,0 +1,29 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return(
+      knex.schema.createTable('users', users => {
+          users.increments('users_id');
+          users.string('username', 255).notNullable().unique();
+          users.string('password', 255).notNullable();
+          users.string('phoneNumber', 15).notNullable().unique();
+      })
+
+      .createTable('plants', plants => {
+          plants.increments('plants_id');
+          plants.string('nickname', 255).notNullable().unique();
+          plants.string('species', 255).notNullable();
+          plants.integer('h2oFrequency').notNullable();
+      })
+  )
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};
