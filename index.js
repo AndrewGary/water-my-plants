4b78da2e278ffe32ce27b1b45fd2f349127270f9
Andrew Gary <andrew.gary91@gmail.com>
2022-04-06 22:01:08 -0500

    wip

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const path = require('path');
 const server = require('./api/server');
 
 
-server.use(express.static(path.join('C:/Users/andre/OneDrive/Desktop/lambdaAssignments/Unit4/BuildWeek/water-my-plants', 'client/build')))
+server.use(express.static(path.join(__dirname, 'client/build')))
 
 const PORT = process.env.PORT || 9000;
 
 server.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'src/index.html'))
+    console.log('drrrrrrrr: ', __dirname);
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
     // res.status(200).json({ message: 'help'})
 })
 
