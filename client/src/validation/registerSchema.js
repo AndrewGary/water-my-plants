a18be613702260b1958b7d23241e4a5fecc5bd7f
Andrew Gary <andrew.gary91@gmail.com>
2022-04-07 17:40:02 -0500

    'WIP'

--- client/src/validation/registerSchema.js
+++ client/src/validation/registerSchema.js
@@ -4,13 +4,13 @@ const SignUpSchema = yup.object().shape({
   username: yup.string().required().min(5),
   password: yup.string().required().min(5),
   phoneNumber: yup
-    .string()
-    .test(
-      "has-10-digits",
-      "phone number needs to be a 10 digit number",
-      (value) => {
-        const digitsOnlyString = value.replace(/[^0-9]/g, "");
-        return digitsOnlyString.length === 10;
-      }
-    ).required,
+    .string().required()
+    // .test(
+    //   "has-10-digits",
+    //   "phone number needs to be a 10 digit number",
+    //   (value) => {
+    //     const digitsOnlyString = value.replace(/[^0-9]/g, "");
+    //     return digitsOnlyString.length === 10;
+    //   }
+    // ).required,
 });
